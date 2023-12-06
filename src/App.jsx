64f5d0a0cefe80183ecf022ccf5d3c5fd0d1d05a
mamaheshd <mamaheshd@gmail.com>
2023-12-06 14:52:17 +0545

    edit in app.js file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// this is jsx code 
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Js </h1>
+    </div>
+  );
+}
+
+export default App;
