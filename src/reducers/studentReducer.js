8ba7c71ddc8a150afc1a15f9dbb965c6ccd86f22
mamaheshd <mamaheshd@gmail.com>
2023-12-10 13:23:33 +0545

    crationg reduser to transfer data from component to reducer

---
+++ src/reducers/studentReducer.js
@@ -0,0 +1,12 @@
+const initialValue={
+    student_name:"mahesh dahal"
+}
+const stusentReducer=(state=initialValue,action)=>{
+    switch(action.type){
+        
+        default:
+            return state
+    }
+}
+
+export default stusentReducer
