c288dcf95eb76befc7788b8dbf1427ba79fbf5db
mamaheshd <mamaheshd@gmail.com>
2023-12-24 13:41:32 +0545

    Python intro

---
+++ python/intro.py
@@ -0,0 +1,38 @@
+print('Hello world ')
+print('Welcome')
+print(23)
+
+# variable
+x=20
+print(x)
+print('the value of x is',x)
+print(f'the value of x is {x}')
+
+# data type 
+# integer
+# float
+# string
+# list->[1,2,3,4,1]
+# dictionary
+# complex no
+# tuple->(1,2,3,4,1)
+# set{1,2,3,4,5}
+# boolean
+
+# to check data type
+print(type(x))
+
+items={
+    'name':'tata',
+    'model':'2021'
+}
+print(items)
+
+x=[1,2,3,4,1,2]
+print(x)
+x=(1,2,3,4,1,2)
+print(x)
+x=1+2j
+print(x)
+x={1,2,3,4,1,2}
+print(x)
