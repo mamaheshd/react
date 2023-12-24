02c93fa1fb9f54c74ed69d5ddef8c16a85ec7b28
mamaheshd <mamaheshd@gmail.com>
2023-12-24 13:58:20 +0545

    Python start  operator

--- python/intro.py
+++ python/intro.py
@@ -9,15 +9,15 @@ print('the value of x is',x)
 print(f'the value of x is {x}')
 
 # data type 
-# integer
+# integer (int)
 # float
-# string
+# string (str)
 # list->[1,2,3,4,1]
-# dictionary
+# dictionary (dict)
 # complex no
 # tuple->(1,2,3,4,1)
 # set{1,2,3,4,5}
-# boolean
+# boolean (bool) -> True , False
 
 # to check data type
 print(type(x))
@@ -36,3 +36,20 @@ x=1+2j
 print(x)
 x={1,2,3,4,1,2}
 print(x)
+
+# arthmetic operator
+# +,-,*,/,//(floor division),%
+
+# assignment operator
+# +=, -=, 
+# x//=y  -> x=x//y
+
+# comparision operator
+# <,>,==,!=  etc
+
+# logical operator
+# or, and, not
+
+# binary operator
+# bitwise AND (&), bitwise OR (|), bitwise XOR(^)
+
