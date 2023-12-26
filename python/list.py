1560182aff404d796b97be47fb9d322ea6d915d6
mamaheshd <mamaheshd@gmail.com>
2023-12-26 13:37:15 +0545

    python list complete

--- python/list.py
+++ python/list.py
@@ -21,8 +21,8 @@ print(fruits)
 fruits.pop(1)
 print(fruits)
 
-fruits1=['apple','cherry','papaya','kiwi']
-fruits1.remove('apple')
+fruits1=['apple','cherry','papaya','kiwi','lol']
+# fruits1.remove('apple')
 print(fruits1)
 
 # clear() this method will empties the list
@@ -48,3 +48,44 @@ print(z)
 
 nums=list(range(10,101,10))
 print(nums)
+
+# sort() 
+fruits.sort() # ascending order
+print(fruits)
+
+fruits.sort(reverse=True) #descending order
+print(fruits)
+
+fruits1.reverse()
+print(fruits)
+
+# tuple-> similar to list but immutable ie element inside tuple cannot be changed and value are in the order and allow duplicate values
+t=('apple','apple','cherry','papaya','kiwi','lol')
+print(t)
+print(len(t))
+print(t[3])
+print(t[-2])
+print(t[1:5])
+print(t[:5])
+
+#count()
+print(t.count('apple'))
+print(t.count('orange'))
+
+
+#index()
+print(t.index('kiwi'))
+
+a=list(t)
+print(a)
+
+
+#nested list
+list1=[10,20,30,40,50]
+list2=[1,2,3,4,5]
+matrix=[list1,list2]
+print(matrix)
+print(matrix[0])
+print(matrix[0][2])
+
+
