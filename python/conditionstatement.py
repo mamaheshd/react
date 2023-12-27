ebe6c30011b79ae55af6c972c4716d0879171f65
mamaheshd <mamaheshd@gmail.com>
2023-12-27 13:43:23 +0545

    python Conditional statement

--- python/conditionstatement.py
+++ python/conditionstatement.py
@@ -1,28 +1,28 @@
-# # if 
-# # if else 
-# # if elif else
-# num=int(input('Enter any values : ' ))
-# if num%2==0:
-#     print(num, 'is an even number.')
-# else:
-#     print(num,'odd no')
+# if 
+# if else 
+# if elif else
+num=int(input('Enter any values : ' ))
+if num%2==0:
+    print(num, 'is an even number.')
+else:
+    print(num,'odd no')
 
-# x=int(input('Enter any values : ' ))
-# y=int(input('Enter any values : ' ))
-# z=int(input('Enter any values : ' ))
-# if x>y and x>z:
-#     print (x,' is gerater no')
-# elif y>x and y>z:
-#     print (y,' is gerater no')
-# else:
-#     print(z,' is greater no')
+x=int(input('Enter any values : ' ))
+y=int(input('Enter any values : ' ))
+z=int(input('Enter any values : ' ))
+if x>y and x>z:
+    print (x,' is gerater no')
+elif y>x and y>z:
+    print (y,' is gerater no')
+else:
+    print(z,' is greater no')
 
-# #leap years
-# year=int(input('Enter any values of years : ' ))
-# if  year % 4 ==0 and year % 100 != 0 or  year % 400 == 0:
-#     print(f'{year} is a leap year')
-# else:
-#     print(year," is not a leap year")
+#leap years
+year=int(input('Enter any values of years : ' ))
+if  year % 4 ==0 and year % 100 != 0 or  year % 400 == 0:
+    print(f'{year} is a leap year')
+else:
+    print(year," is not a leap year")
 
 nums= int(input('Enter any values : ' ))
 if nums%3==0 and nums%5==0:
---
+++ python/loop.py
@@ -0,0 +1,1 @@
+# loop for and while loop
