a4dd55cc440040457bda6296ffb10ad424c00a1e
mamaheshd <mamaheshd@gmail.com>
2023-12-30 16:09:10 +0545

    lambda expression

---
+++ python/lambda.py
@@ -0,0 +1,7 @@
+# lambda expression allow us to create anonymous function. This basically means we can quickly make ad-hoc function without nedding to properly defined the function using keyword def.
+# def square(num):return(num**2)
+square=lambda num:num**2
+
+result=square(5)
+print(result)
+
