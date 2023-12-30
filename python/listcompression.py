520d1f8fa8bb01840281d6608b3c07f302bb058c
mamaheshd <mamaheshd@gmail.com>
2023-12-30 16:03:08 +0545

    map and filter expression in python

--- python/listcompression.py
+++ python/listcompression.py
@@ -14,3 +14,20 @@ print(number)
 cel=[10,20,30,40,50,60,70,80,90,100]
 f=[(9/5)*c+32 for c in cel]
 print(f)
+
+
+# map 
+def squareFunc(num):
+    return num**2
+
+numbers=[2,3,4,5,6]
+result=list(map(squareFunc,numbers))
+print(result)
+
+#filter
+def filterFunc(num):
+    return num==2
+
+numbers=[2,3,4,5,6]
+result=list(filter(filterFunc,numbers))
+print(result)
