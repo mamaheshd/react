3f5d21e6149eb9607061d13e7823e8359500ddff
mamaheshd <mamaheshd@gmail.com>
2023-12-31 13:27:06 +0545

    recursive function to calculate factorial of no

--- python/ternaryoperator.py
+++ python/ternaryoperator.py
@@ -1,3 +1,14 @@
 number=int(input('Enter the number:'))
 msg='even' if number%2==0 else 'odd'
 print(msg)
+
+# recursive function
+def fact(n):
+    if(n==0):
+        return 1
+    else:
+        return n*fact(n-1)
+    
+n=int(input('enter a no to calculate factorial'))
+f=fact(n)
+print(f)
