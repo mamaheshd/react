e4ab54b9238de60d866e1feffcd81fcd52b4369a
mamaheshd <mamaheshd@gmail.com>
2024-01-01 20:19:13 +0545

    bug fixing

--- python/filehandeling.py
+++ python/filehandeling.py
@@ -5,14 +5,14 @@ print(myfile)
 print(myfile.read())
 
 #if you went to print the pragraph from certain index number then do the following using seek()
-myfile.srrk(6)
+myfile.seek(6)
 print(myfile.read())
 
-myfile.srrk(0)
+myfile.seek(0)
 print(myfile.read())
 
 #readline(): it will return the list of the lines in the file
-myfile.srrk(0)
+myfile.seek(0)
 print(myfile.readline())
 
 myfile.close()
@@ -24,6 +24,11 @@ wfile.write('This is python program')
 wfile.write('\nThis is new line in test files')
 wfile.close()
 
-
+#create a new file using w if the file is not exist
+newfile=open('abc.txt','w')
+print(newfile)
+newfile.write('This is python program')
+newfile.write('\nThis is new line in test files')
+newfile.close()
 
 
