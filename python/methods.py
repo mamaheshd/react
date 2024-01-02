2dadc9a974dd52aeb87f135d21603ea27197834e
mamaheshd <mamaheshd@gmail.com>
2024-01-02 14:01:29 +0545

    defining the own method inside class

--- python/methods.py
+++ python/methods.py
@@ -7,9 +7,16 @@ class Circle:
     def __init__(self,radius=5):
         self.radius=radius
         self.area=Circle.pi*radius*radius
-
+    # defining the own method
+    def setRadius(self,newradius):
+        self.radius=newradius
+        self.area=self.pi*newradius*newradius
+        
 
 c1=Circle()
 print('The area is ',c1.area)
 
-
+# calling the method
+c1.setRadius(10)
+print('New radius is ',c1.radius)
+print('The area is ',c1.area)
