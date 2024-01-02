752ed213cdd9f8c0350881492179974f714fe5b8
mamaheshd <mamaheshd@gmail.com>
2024-01-02 13:28:16 +0545

    class and objects  basic

---
+++ python/classandobject.py
@@ -0,0 +1,22 @@
+# class and objects 
+# class blueprint that defines the nature of the future object or you can simply say collection of objects.
+#int,folat,list,tuple etc
+
+#to create own class
+class Test:
+    pass
+
+class Student:
+    pass
+
+obj_mahesh=Student()
+print(type(obj_mahesh))
+
+obj_anusha=Student()
+print(obj_anusha)
+
+class Car:
+    pass
+
+obj_bmw=Car()
+
