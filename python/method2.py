7c529007d09b6b788b9b9fefa21f117c4fd8a359
mamaheshd <mamaheshd@gmail.com>
2024-01-03 21:18:33 +0545

    instance method

---
+++ python/method2.py
@@ -0,0 +1,2 @@
+# instance method  class method and static method
+# class method : methods that are not bound to be an instance of class and class method can only access and modify class variable but not instance variable
