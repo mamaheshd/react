1d27b79bd1dd7eb5f2093411e2346a6b11cd613c
mamaheshd <mamaheshd@gmail.com>
2024-01-07 17:50:53 +0545

    theory of  method

--- python/method2.py
+++ python/method2.py
@@ -1,4 +1,10 @@
 # instance method  class method and static method 
+
 # class method : methods that are not bound to be an instance of class and class method can only access and modify class variable but not instance variable
 
-# instance method: 
+# instance method:  can access variable as well as instance variable 
+
+#static methods: can't access class attribute or instance attribute and static method is define using @staticmethod decorator
+
+# example
+
