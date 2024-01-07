28cfd275c61c702a4e11bd588adad31ed69f954b
mamaheshd <mamaheshd@gmail.com>
2024-01-07 18:24:34 +0545

    instance variable example

--- python/method2.py
+++ python/method2.py
@@ -6,5 +6,22 @@
 
 #static methods: can't access class attribute or instance attribute and static method is define using @staticmethod decorator
 
-# example
+class Account:
+    minbal=1000 # class variable 
 
+    def __init__(self,accno,name,balance):
+        self.accno=accno #instance variable
+        self.name=name
+        self.balance=balance
+
+    def display(self):
+        print('Account Number : ',self.accno)
+        print('Account Holder Name : ',self.name)
+        print('Available Balance : ',self.balance)
+
+
+
+a=Account(2001,'Mahesh',50000)
+b=Account(2002,'Dahal',75000)
+# a.displayMinBal()
+a.display()
