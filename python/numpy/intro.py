e62e81f8a0e88e5a41f07412f8f5137d47532c05
mamaheshd <mamaheshd@gmail.com>
2024-01-08 17:47:48 +0545

    numpy intro linear algebra

--- python/numpy/intro.py
+++ python/numpy/intro.py
@@ -32,3 +32,45 @@ print(z)
 #arnge()
 c=np.arange(10)
 print(c)
+
+r=np.array([[1.,2.,3.],[4.,5.,6.]])
+print(r)
+
+# element wise multiplication
+result=r*r
+print(result)
+
+#element wise addition
+result=r+r
+print(result)
+
+c=np.arange(1,10)
+print(c[4]) #indexing
+print(c[3:8]) #slicing
+
+#boolean indexing
+a=np.array([12,24,16,27,30,40,43,7])
+boolean_mask=a>20
+print(a[boolean_mask])
+
+#fancy indexing
+fancy_indexing=a[[1,4,5]]
+print(fancy_indexing)
+
+# unary  universal function 
+a=np.arange(10)
+s=np.sqrt(a)
+print(s)
+
+#linear algebra
+x=np.array([[1,2,3],[4,5,6]])
+y=np.array([[6,12],[1,5],[8,9]])
+#matrix multiplication
+result=x.dot(y)
+print(result)
+r=np.dot(x,y)
+print(r)
+
+# transpose
+r=np.transpose(x)
+print(r)
