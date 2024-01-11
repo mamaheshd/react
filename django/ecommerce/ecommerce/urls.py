417945fcb746f750056b00309a7f68a833da2637
mamaheshd <mamaheshd@gmail.com>
2024-01-11 13:55:03 +0545

    in app product function are create in view and manage urls

--- django/ecommerce/ecommerce/urls.py
+++ django/ecommerce/ecommerce/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.http import HttpResponse # data comes from web to user
 
 def index(request):
@@ -28,4 +28,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', index),
     path('display/',display),
+    path('product/',include('product.urls'),)
 ]
