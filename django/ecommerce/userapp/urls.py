0f77ba7467508f6df8aad99919f1fd7b8eb73011
mamaheshd <mamaheshd@gmail.com>
2024-01-25 14:00:46 +0545

    product show and view details of the products

--- django/ecommerce/userapp/urls.py
+++ django/ecommerce/userapp/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('',index),
+    path('productdetails/<int:product_id>',product_details),
+    path('productlist/',products),
 ]
--- django/ecommerce/userapp/views.py
+++ django/ecommerce/userapp/views.py
@@ -4,7 +4,21 @@ from product.models import Product
 # Create your views here.
 def index(request):
     products=Product.objects.all().order_by('-id')[:8]
-    contex={
+    context={
         'products':products
     }
-    return render(request,'user/index.html',contex)
+    return render(request,'user/index.html',context)
+
+def product_details(request,product_id):
+    product=Product.objects.get(id=product_id)
+    context={
+        'product':product
+    }
+    return render(request,'user/productdetails.html',context)
+
+def products(request):
+    products=Product.objects.all().order_by('-id')
+    context={
+        'products':products
+    }
+    return render(request,'user/products.html',context)
