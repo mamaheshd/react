0df0013ba29f503b7489a533db8eb101ebdeb649
mamaheshd <mamaheshd@gmail.com>
2024-02-08 21:50:44 +0545

    changing database

--- django/ecommerce/ecommerce/settings.py
+++ django/ecommerce/ecommerce/settings.py
@@ -86,8 +86,14 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'db_django_project',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'3306',
+        'OPTIONS':{
+            'init_comment':"SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
