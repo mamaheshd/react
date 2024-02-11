332aeb65af05c0496385345106dd0178bcd11041
mamaheshd <mamaheshd@gmail.com>
2024-02-11 09:26:33 +0545

    Rest API creation for post and user can vote in that post

---
+++ django/restapi/project/posts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .models import *
+from rest_framework import generics,permissions,mixins,status
+from .serializers import *
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+
+# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    queryset=Post.objects.all()
+    serializer_class=PostSerializer
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
+
+class VoteCreate(generics.CreateAPIView,mixins.DestroyModelMixin):
+    serializer_class=VoteSerializer
+    permission_classes=[permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user=self.request.user
+        post=Post.objects.get(pk=self.kwargs['pk'])
+        return Vote.objects.filter(voter=user,post=post)
+
+    def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('You have already voted for this post')
+        serializer.save(voter=self.request.user,post=Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self,request,*args,**kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError('You never voted for this post')
